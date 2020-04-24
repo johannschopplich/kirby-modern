578f7dff7999697cee37f2b28236cfbf11f22eae
Johann Schopplich <mail@johannschopplich.com>
2020-04-24 15:12:43 +0200

    Initial commit

---
+++ site/config/config.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * The config file is optional. It accepts a return array with config options
+ * Note: Never include more than one return statement, all options go within this single return array
+ * In this example, we set debugging to true, so that errors are displayed onscreen.
+ * This setting must be set to false in production.
+ * All config options: https://getkirby.com/docs/reference/system/options
+ */
+
+$base = dirname(__DIR__, 2);
+(new \Beebmx\KirbyEnv($base))->load();
+
+return [
+
+    'debug' => env('KIRBY_DEBUG', false),
+
+    'panel' => [
+        'install' => env('KIRBY_PANEL_INSTALL', false)
+    ],
+
+    'routes' => require 'routes.php',
+
+    'cache' => [
+        'pages' => [
+            'active' => env('KIRBY_CACHE', false),
+            'ignore' => function ($page) {
+                $options = $page->blueprint()->options();
+                return isset($options['cache']) ? !$options['cache'] : false;
+            }
+        ]
+    ],
+
+    'd4l' => [
+        'static_site_generator' => [
+          'endpoint' => 'generate-static-site',
+          'output_folder' => '../static',
+          'preserve' => [], # anything starting with `.` is always preserved
+          'base_url' => '/',
+          'skip_media' => false,
+          'skip_templates' => []
+        ]
+    ],
+
+    'cre8ivclick.sitemapper.title' => 'Sitemap'
+
+];
---
+++ site/config/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    [
+        'pattern' => 'robots.txt',
+        'method' => 'ALL',
+        'action' => function () {
+            $robots = 'User-agent: *' . PHP_EOL;
+            $robots .= 'Allow: /' . PHP_EOL;
+            $robots .= 'Sitemap: ' . url('sitemap.xml');
+            return kirby()
+                ->response()
+                ->type('text')
+                ->body($robots);
+        }
+    ]
+];
